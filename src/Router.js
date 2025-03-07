8195ebfbc81623ee8484eec51d91776518d156e5
marksenee <mark233891@gmail.com>
2025-03-08 01:50:05 +0900

    [Bug] 로그인 페이지 빈 화면

--- src/Router.js
+++ src/Router.js
@@ -1,50 +1,23 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./Layout";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import NavigationTabs from "./components/tab/Tab";
 import AdminNavigationTabs from "./components/tab/AdminTab";
-import useAuthStore from "./\bstore/useAuthStore";
-
-const ProtectRouter = ({ children }) => {
-  const { username } = useAuthStore();
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    // 세션 스토리지가 제대로 반영될 때까지 로딩 유지
-    const timeout = setTimeout(() => setLoading(false), 100);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  if (loading) return <div>로딩 중...</div>;
-
-  if (!username) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
-};
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 로그인 페이지 */}
+        {/* 로그인 화면 */}
         <Route path="/login" element={<LoginPage />} />
 
-        {/* 로그인 필요 페이지 */}
-        <Route
-          path="/"
-          element={
-            <ProtectRouter>
-              <Layout />
-            </ProtectRouter>
-          }
-        >
-          <Route index element={<MainPage />} />
-          <Route path="checklist/*" element={<NavigationTabs />} />
-          <Route path="admin/*" element={<AdminNavigationTabs />} />
+        {/* 로그인 후에 접근해야 할 페이지 */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/checklist/*" element={<NavigationTabs />} />
+          <Route path="/admin/*" element={<AdminNavigationTabs />} />
         </Route>
       </Routes>
     </BrowserRouter>
