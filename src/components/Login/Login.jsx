9f9f14a6e5cff77aea61ce9a3a7a2b7978ad2da0
marksenee <mark233891@gmail.com>
2025-03-08 00:59:10 +0900

    [Delete] 로그인 페이지 주석

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,89 +1,89 @@
-import React from "react";
-import styled from "styled-components";
+// import React from "react";
+// import styled from "styled-components";
 
-// 전체 화면 중앙 정렬
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f8f9fa;
-`;
+// // 전체 화면 중앙 정렬
+// const Container = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   height: 100vh;
+//   background-color: #f8f9fa;
+// `;
 
-// 로그인 박스
-const LoginBox = styled.div`
-  width: 704px;
-  height: 500px;
-  border: 2px solid #dcdcdc;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
+// // 로그인 박스
+// const LoginBox = styled.div`
+//   width: 704px;
+//   height: 500px;
+//   border: 2px solid #dcdcdc;
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   justify-content: center;
+// `;
 
-// 로그인 텍스트
-const LoginTitle = styled.h1`
-  font-family: "SUITE", sans-serif;
-  font-size: 50px;
-  font-weight: bold;
-  color: #ff7710;
-  /* margin-bottom: 50px; */
-`;
+// // 로그인 텍스트
+// const LoginTitle = styled.h1`
+//   font-family: "SUITE", sans-serif;
+//   font-size: 50px;
+//   font-weight: bold;
+//   color: #ff7710;
+//   /* margin-bottom: 50px; */
+// `;
 
-// 입력창 스타일
-const Input = styled.input`
-  width: 506px;
-  height: 93px;
-  border: 2px solid #ff7710;
-  font-size: 18px;
-  padding: 10px;
-  margin-bottom: 20px;
-  outline: none;
+// // 입력창 스타일
+// const Input = styled.input`
+//   width: 506px;
+//   height: 93px;
+//   border: 2px solid #ff7710;
+//   font-size: 18px;
+//   padding: 10px;
+//   margin-bottom: 20px;
+//   outline: none;
 
-  &::placeholder {
-    color: gray;
-  }
-`;
+//   &::placeholder {
+//     color: gray;
+//   }
+// `;
 
-// 로그인 버튼
-const LoginButton = styled.button`
-  width: 506px;
-  height: 93px;
-  background-color: #ff7710;
-  color: white;
-  font-size: 20px;
-  font-weight: bold;
-  border: none;
-  cursor: pointer;
+// // 로그인 버튼
+// const LoginButton = styled.button`
+//   width: 506px;
+//   height: 93px;
+//   background-color: #ff7710;
+//   color: white;
+//   font-size: 20px;
+//   font-weight: bold;
+//   border: none;
+//   cursor: pointer;
 
-  &:hover {
-    background-color: #e0660d;
-  }
-`;
+//   &:hover {
+//     background-color: #e0660d;
+//   }
+// `;
 
-const Login = () => {
-  return (
-    <Container>
-      <LoginBox>
-        {/* 로고 */}
-        <h1 style={{ fontSize: "30px", marginBottom: "20px" }}>
-          멋쟁이사자처럼
-        </h1>
+// const Login = () => {
+//   return (
+//     <Container>
+//       <LoginBox>
+//         {/* 로고 */}
+//         <h1 style={{ fontSize: "30px", marginBottom: "20px" }}>
+//           멋쟁이사자처럼
+//         </h1>
 
-        {/* 로그인 제목 */}
-        <LoginTitle>로그인</LoginTitle>
+//         {/* 로그인 제목 */}
+//         <LoginTitle>로그인</LoginTitle>
 
-        {/* ID 입력창 */}
-        <Input type="text" placeholder="ID를 입력해주세요(사번)" />
+//         {/* ID 입력창 */}
+//         <Input type="text" placeholder="ID를 입력해주세요(사번)" />
 
-        {/* 비밀번호 입력창 */}
-        <Input type="password" placeholder="비밀번호를 입력해주세요" />
+//         {/* 비밀번호 입력창 */}
+//         <Input type="password" placeholder="비밀번호를 입력해주세요" />
 
-        {/* 로그인 버튼 */}
-        <LoginButton>로그인</LoginButton>
-      </LoginBox>
-    </Container>
-  );
-};
+//         {/* 로그인 버튼 */}
+//         <LoginButton>로그인</LoginButton>
+//       </LoginBox>
+//     </Container>
+//   );
+// };
 
-export default Login;
+// export default Login;
