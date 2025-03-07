563e4d0546527988319cb59afa9c16c9b62e769c
marksenee <mark233891@gmail.com>
2025-03-08 01:41:05 +0900

    [Bug] 로그인 페이지 빈 화면

--- src/Router.js
+++ src/Router.js
@@ -1,27 +1,42 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./Layout";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import NavigationTabs from "./components/tab/Tab";
 import AdminNavigationTabs from "./components/tab/AdminTab";
-import ProtectRouter from "./ProtectRouter";
+import useAuthStore from "./store/useAuthStore";
+
+const ProtectRouter = ({ children }) => {
+  const { username } = useAuthStore();
+
+  if (!username) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        {/* ✅ 로그인 필요 페이지 (ProtectedRoute로 감싸기) */}
-        {/* <Route element={<ProtectRouter />}> */}
-        <Route element={<Layout />}>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/checklist/*" element={<NavigationTabs />} />
-          <Route path="/admin/*" element={<AdminNavigationTabs />} />
-        </Route>
-        {/* </Route> */}
-
-        {/* 개별적으로 관리할 페이지 (헤더 제외) */}
+        {/* 로그인 페이지 */}
         <Route path="/login" element={<LoginPage />} />
+
+        {/* 로그인 필요 페이지 */}
+        <Route
+          path="/"
+          element={
+            <ProtectRouter>
+              <Layout />
+            </ProtectRouter>
+          }
+        >
+          <Route index element={<MainPage />} />
+          <Route path="checklist/*" element={<NavigationTabs />} />
+          <Route path="admin/*" element={<AdminNavigationTabs />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
