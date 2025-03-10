ada952c3d65b68961beaa4ff2af900a357dcd974
marksenee <mark233891@gmail.com>
2025-03-10 13:39:16 +0900

    [Fix] 디자인 수정

--- src/components/tab/styles.jsx
+++ src/components/tab/styles.jsx
@@ -23,7 +23,7 @@ const TabContainer = styled.div`
 `;
 
 const TabItem = styled.div`
-  font-size: 25px;
+  font-size: 22px;
   color: ${(props) => (props.active ? "#FF7710" : "#888888")};
   cursor: pointer;
   position: relative;
